553d6e47cd7314ad43c4c40a00f4be5e4e0c0309
queiroz.rafaelreis@gmail.com <queiroz.rafaelreis@gmail.com>
2017-11-06 22:40:30 -0200

    Ajuste do plugin sdk facebook

--- rodape.php
+++ rodape.php
@@ -15,7 +15,17 @@
 				<a target="_blank" title="siga no facebook" href="https://www.facebook.com/happytripbrasil/?fref=ts"><i class="fa fa-facebook-square fa-2x"></i></a>
 				<a target="_blank" title="siga no instagram" href="https://www.instagram.com/happytripbrasil/?hl=pt-br"><i class="fa fa-instagram fa-2x"></i></a>
 			</p>
-			<div class="fb-like _2tga" data-href="https://www.facebook.com/happytripbrasil/" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
+			<div id="fb-root"></div>
+			<script>
+			(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10';
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+			</script>
+			<div class="fb-like" data-href="https://www.facebook.com/happytripbrasil/" data-layout="button" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
 	</div>
 	<div class="four columns">
 		<span><img class="newsmargin float logo-cadastur" src="images/logos/cadastur.png" alt=""></span>
